9c627de260ea2b8eafe188c3da688e7912ee3387
Aaron Diep <aaron@delite.ca>
2018-08-20 18:50:46 -0600

    using oops

--- tangd/src/tangd/controller/rec.cljs
+++ tangd/src/tangd/controller/rec.cljs
@@ -1,12 +1,12 @@
 (ns tangd.controller.rec
-(:require
- [oops.core :as oops]))
+  (:require
+   [oops.core :as oops]))
 
 
 (defn respond-kid [req res next]
   (do
     (oops/oset! res "!contentType" "json")
-    (.send res  #js {:kid (oops/oget req :params :kid)
-                    :name (oops/oget req :body :name)})
+    (oops/ocall res :send #js {:kid (oops/oget req :params :kid)
+                               :name (oops/oget req :body :name)})
     (next false)))
 

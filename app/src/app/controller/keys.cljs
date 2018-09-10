d0cc020402a5785e0751c842c94a33474f7052fc
Aaron Diep <aaron@delite.ca>
2018-09-10 14:21:41 -0600

    factored out error handling

--- app/src/app/controller/keys.cljs
+++ app/src/app/controller/keys.cljs
@@ -11,42 +11,46 @@
   [(fn [success] (rf/dispatch [:http-response {:status :NO_CONTENT} ->context]))
    (fn [err] (if err
                (do (println err)
-                   (rf/dispatch [:http-response {:status :METHOD_FAILURE :error err} ->context])     ) 
+                   (rf/dispatch [:http-response
+                                 {:status :METHOD_FAILURE :error err} ->context]))
                err))])
 
 (defn rotate-keys [db ->context]
   (let [[es512 ecmr payload jws] (get-in ->context [:jose :init-vals])
         [success error] (restify-handlers ->context)]
     #_(println "rotating keys")
-    (go
-      (-> (go-try
-           (-> (keys/begin-transaction db)
-               (<?) ((fn [_](keys/insert-jwk db ecmr)))
-               (<?) ((keys/insert-thp db ecmr))
-               (<?) ((fn [_] (keys/insert-jwk db es512)))
-               (<?) ((keys/insert-thp db es512))
-               (<?) ((fn [_] (keys/drop-jws-table db)))
-               (<?) ((fn [_] (keys/create-jws-table db)))
-               (<?) ((fn [_] (keys/create-jws-jwk-index db)))
-               (<?) ((fn [_] (keys/select-all-jwk db)))
-               (#(let [insert-func (keys/insert-jws db payload es512)]
-                   (go-try
-                    (loop [doc (<? %)]
-                      (if (number? doc) doc
-                          (do (insert-func doc)
-                              (recur (<? %))))))))
-               (<?) (#(if %
-                        (do (keys/commit-transaction db) (success %))
-                        (do (keys/rollback-transaction db) (success "rollback."))))))
-          (<!) error))))
+    (go-try
+     (-> (keys/begin-transaction db)
+         (<?) ((fn [_](keys/insert-jwk db ecmr)))
+         (<?) ((keys/insert-thp db ecmr))
+         (<?) ((fn [_] (keys/insert-jwk db es512)))
+         (<?) ((keys/insert-thp db es512))
+         (<?) ((fn [_] (keys/drop-jws-table db)))
+         (<?) ((fn [_] (keys/create-jws-table db)))
+         (<?) ((fn [_] (keys/create-jws-jwk-index db)))
+         (<?) ((fn [_] (keys/select-all-jwk db)))
+         (#(let [insert-func (keys/insert-jws db payload es512)]
+             (go-try
+              (loop [doc (<? %)]
+                (if (number? doc) doc
+                    (do (insert-func doc)
+                        (recur (<? %))))))))
+         (<?) (#(if %
+                  (do (keys/commit-transaction db) (success %))
+                  (do (keys/rollback-transaction db) (success "rollback."))))))))
 
+
+(defn try-response [fn & params])
 
 (coop/restify-route-event
  :rotate-keys
  (fn [{:keys [db]} [->context]]
    (let [init-vals (keys/rotate-keys)
          ->context (assoc-in ->context [:jose :init-vals] init-vals)
-         [es512 ecmr payload jws] init-vals]
-     (rotate-keys (get-in db [:sqlite :db]) ->context)
+         [es512 ecmr payload jws] init-vals
+         [success error] (restify-handlers ->context)
+         sqlite-db (get-in db [:sqlite :db])]
+     (go (-> (rotate-keys sqlite-db ->context)
+             (<!) error))
      {:db (assoc-in db [:jose] {:default-jws jws :payload payload})})))
 

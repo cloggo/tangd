b3ec98d8270c219be2e5af7869ab9c34e27724c4
Aaron Diep <aaron@delite.ca>
2018-09-21 05:14:17 +0000

    Delete hello.cljs

--- app/src/app/controller/hello.cljs
+++
@@ -1,9 +0,0 @@
-(ns app.controller.hello
-  (:require
-   [oops.core :as oops]))
-
-
-(defn respond [req res next*]
-  (do
-    (.send res (str "hello " (oops/oget req :params :name)))
-    (next* false)))
